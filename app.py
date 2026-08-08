79f883fadd3d47fae80abb91695a384bb5b80e52
JSaulRivera <josesaulriveralopez@gmail.com>
2026-08-08 11:59:16 -0600

    Corregir posiciones

--- app.py
+++ app.py
@@ -26,11 +26,11 @@ os.makedirs("capturas_entrada", exist_ok=True)
 os.makedirs("capturas_salida", exist_ok=True)
 
 zonas = {
-    '1': ([(3, 3), (3, 100), (85, 87), (85, 3)], (5, 10)),
-    '2': ([(90, 3), (90, 87), (170, 72), (170, 3)], (95, 10)),
-    '3': ([(175, 3), (175, 71), (230, 64), (230, 3)], (180, 10)),
-    '4': ([(235, 3), (235, 64), (300, 57), (300, 3)], (240, 10)),
-    '5': ([(305, 3), (305, 57), (365, 50), (365, 3)], (310, 10)),
+    '1': ([(3, 65), (3, 150), (75, 133), (75, 60)], (5, 10)),
+    '2': ([(80, 60), (80, 130), (135, 115), (135, 55)], (95, 10)),
+    '3': ([(140, 52), (140, 113), (200, 95), (200, 42)], (180, 10)),
+    '4': ([(205, 40), (205, 93), (290, 68), (290, 25)], (240, 10)),
+    '5': ([(295, 25), (295, 63), (350, 50), (350, 20)], (310, 10)),
     '6': ([(515, 55), (470, 90), (555, 120), (555, 55)], (490, 60)),
     '7': ([(465, 95), (400, 140), (530, 170), (550, 125)], (450, 95)),
     '8': ([(395, 146), (300, 200), (480, 250), (523, 178)], (370, 155)),
@@ -55,75 +55,14 @@ DISTANCIA_MAXIMA_MISMO_OBJETO = 50
 track_history = defaultdict(lambda: [])
 puntos = []
 
-# def click_event(event, x, y, flags, param):
-#     if event == cv2.EVENT_LBUTTONDOWN:
-#         puntos.append((x, y))
-#         print(f"Punto agregado: ({x}, {y})")
-#         if len(puntos) >= 2:
-#             cv2.line(param, puntos[-2], puntos[-1], (255, 0, 0), 2)
 
-# def registrar_evento_csv(nombre_archivo, datos):
-#     with open(nombre_archivo, mode='a', newline='') as archivo:
-#         writer = csv.writer(archivo)
-#         writer.writerow(datos)
-#         formato = '%Y-%m-%d %H:%M:%S'
-#         fecha_datetime = datetime.datetime.strptime(datos[3], formato)
-       
-#         if datos[5]=='':
-#             tiempo_estacionado=0
-#             tiempo_formato='00:00:00'
-#         else:
-#             tiempo_estacionado=int(datos[5])
-#             tiempo_formato=datos[4]
-#         gis = GIS()
-#         lyr_url = 'https://smart-twins.sigsa.info/server/rest/services/Hosted/RegistroEstacionamiento_WFL1/FeatureServer/0'
-#         layer = FeatureLayer(lyr_url)
-#         new_point = {
-#             'attributes': {
-#                 'evento': datos[0],
-#                 'id': datos[1],
-#                 'zona': datos[2],
-#                 'fechaevento': fecha_datetime,                
-#                 'imagen': datos[6],
-#                 'tiempoestacionado':tiempo_estacionado,
-#                 'tiempoduracion': tiempo_formato,
-#                 'disponibilidad': 13-datos[7]
-#             },
-#             'geometry': {
-#             'x': -98.414301,
-#             'y': 20.063116,
-#             'spatialReference': {'wkid': 4326}
-#         },
-#             'visible': True
-#         }
-#         nuevo_feature = Feature(geometry=new_point['geometry'], attributes=new_point['attributes'])
-#         try:
-#             resultado = layer.edit_features(adds=[nuevo_feature])
-#             if resultado and 'addResults' in resultado and resultado['addResults']:
-            
-#                 if 'success' in resultado['addResults'][0]:
-#                     if datos[0]=='ENTRADA':
-#                         valevento="capturas_entrada/"
-#                     else:
-#                         valevento="capturas_salida/"
-
-#                     new_object_id = resultado['addResults'][0]['objectId']
-#                     attachment_result = layer.attachments.add(
-#                     new_object_id,valevento+datos[6]  
-#                 )
-#                     if attachment_result:
-#                         print('Imagen adjuntada correctamente.')
-#                     else:
-#                         print('Error al adjuntar la imagen.')
-#                 else:
-#                     print('Error al agregar el punto:', resultado['addResults'][0].get('error', ''))
-#             else:
-#                 print('Error inesperado al agregar el punto')
-#         except Exception as e:
-#             print('Excepción:', str(e))
-
-
-#         print("Datos agregados.")
+def registrar_evento_csv(nombre_archivo, datos):
+    with open(nombre_archivo, mode='a', newline='') as archivo:
+        writer = csv.writer(archivo)
+        writer.writerow(datos)
+        formato = '%Y-%m-%d %H:%M:%S'
+        fecha_datetime = datetime.datetime.strptime(datos[3], formato)
+        print("Datos agregados.")
 
 while cap.isOpened():
     success, frame = cap.read()
@@ -193,8 +132,7 @@ while cap.isOpened():
                             nombre_imagen = f"entrada_{zona}_id{track_id}_{timestamp_str}.jpg"
                             cv2.polylines(frame, [np.array(poligono, np.int32)], True, (255, 0, 0), 1)
                             cv2.imwrite(f"capturas_entrada/{nombre_imagen}", frame)
-                            #threading.Thread(target=registrar_evento_csv, args=("eventos.csv", ["ENTRADA", track_id, zona, hora_exacta_str, "","", nombre_imagen,ocupados])).start()
-                            # registrar_evento_csv("eventos.csv", ["ENTRADA", track_id, zona, hora_exacta_str, "","", nombre_imagen,ocupados])
+                            threading.Thread(target=registrar_evento_csv, args=("eventos.csv", ["ENTRADA", track_id, zona, hora_exacta_str, "","", nombre_imagen,ocupados])).start()
                             estado_zonas[zona]['imagen_guardada'] = True
                             print(f"[ENTRADA] ID {track_id} en zona {zona} (captura retrasada 10s)")
 
@@ -209,7 +147,7 @@ while cap.isOpened():
 
                 segundos = int(tiempo_actual - estado_zonas[zona]['tiempo_entrada'])
                 tiempo_formateado = time.strftime('%H:%M:%S', time.gmtime(segundos))
-                cv2.putText(frame, tiempo_formateado, (texto_pos[0]-10,texto_pos[1]+70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)#cronometro
+                cv2.putText(frame, tiempo_formateado, (texto_pos[0]-20,texto_pos[1]+90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)#cronometro
 
             
             for zona, (poligono, texto_pos) in zonas.items():
@@ -236,7 +174,7 @@ while cap.isOpened():
                 nombre_imagen = f"salida_{zona}_id{datos['track_id']} {timestamp_str}.jpg"
                 cv2.polylines(frame, [np.array(poligono, np.int32)], True, (255, 0, 0), 1)
                 cv2.imwrite(f"capturas_salida/{nombre_imagen}", frame)
-                #threading.Thread(target=registrar_evento_csv, args=("eventos.csv", ["SALIDA", datos['track_id'], zona, hora_exacta_str, duracion_formato,duracion_segundos, nombre_imagen, ocupados])).start()
+                threading.Thread(target=registrar_evento_csv, args=("eventos.csv", ["SALIDA", datos['track_id'], zona, hora_exacta_str, duracion_formato,duracion_segundos, nombre_imagen, ocupados])).start()
                 
                 print(f"[SALIDA] ID {datos['track_id']} salió de zona {zona} (duración: {duracion_formato})")
             else:
@@ -245,10 +183,9 @@ while cap.isOpened():
             estado_zonas[zona] = {'track_id': None, 'tiempo_entrada': None, 'centroide': None, 'ultimo_update': 0, 'imagen_guardada': False}
 
     espacios_disponibles = TOTAL_ESPACIOS - ocupados
-    cv2.putText(frame, f"Disponibles: {espacios_disponibles}/{TOTAL_ESPACIOS}", (10, 330), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 200, 0), 1)
+    cv2.putText(frame, f"Disponibles: {espacios_disponibles}/{TOTAL_ESPACIOS}", (5, 380), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 200, 0), 1)
     cv2.imshow("Estacionamiento", frame)
-    # cv2.setMouseCallback("Estacionamiento", click_event)
-
+   
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
